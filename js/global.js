00a1171a74ea663aa7de34b63c1fe9964814297a
Andy Denham <andywd7@users.noreply.github.com>
2014-09-27 23:48:04 +0100

    overdue update

---
+++ js/global.js
@@ -0,0 +1,10 @@
+﻿jQuery(document).ready(function() {
+    jQuery(function() {
+        //@prepros-append navbar.js
+            new uiSearch(document.getElementById('js-navbarSearch'));
+
+        //@prepros-append jquery-accessibleMegaMenu.js
+            // initialize a selector as an accessibleMegaMenu
+            // jQuery('#navbar-nav').accessibleMegaMenu();
+    });
+});
